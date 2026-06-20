953cda00c40312dbb3cbf16db6dd9e7c7eaa3fab
Mushahid Bashir <mushahidbashir47@gmail.com>
2026-06-20 21:43:23 +0500

    Fix intentionally broken test

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_health():
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.json() == {"status": "broken"}
+    assert response.json() == {"status": "ok"}
 
 
 def test_create_task():
